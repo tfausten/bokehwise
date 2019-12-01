16b9abafa12b2246097871e7b3560d2ba26084b7
tfausten <tobias.fausten@gmail.com>
2019-12-01 14:03:21 +0900

    cleanup and new tab-based layout

--- constants.py
+++ constants.py
@@ -2,11 +2,20 @@ from collections import OrderedDict
 import os
 
 CAT_TO_SUBCAT = OrderedDict()
-CAT_TO_SUBCAT['Rent & Utilities'] = ['Rent', 'Heat/gas', 'TV/Phone/Internet', 'Electricity', 'Water', 'Utilities - Other']
-CAT_TO_SUBCAT['Food & Drink'] = ['Groceries', 'Food and drink - Other', 'Dining out']
-CAT_TO_SUBCAT['Household Supplies'] = ['Household supplies', 'Furniture', 'Electronics', 'Home - Other']
-CAT_TO_SUBCAT['Travel & Entertainment'] = ['Hotel', 'Plane', 'Transportation - Other', 'Entertainment - Other', 'Games', 'Movies', 'Music', 'Sports']
-CAT_TO_SUBCAT['Other'] = ['General', 'Clothing', 'Life - Other', 'Gifts', 'Education']
+CAT_TO_SUBCAT['Rent & Utilities'] = [
+    'Rent', 'Heat/gas', 'TV/Phone/Internet', 'Electricity', 'Water', 'Utilities - Other']
+
+CAT_TO_SUBCAT['Food & Drink'] = [
+    'Groceries', 'Food and drink - Other', 'Dining out']
+
+CAT_TO_SUBCAT['Household Supplies'] = [
+    'Household supplies', 'Furniture', 'Electronics', 'Home - Other']
+
+CAT_TO_SUBCAT['Travel & Entertainment'] = ['Hotel', 'Plane', 'Transportation - Other', 'Taxi',
+                                           'Entertainment - Other', 'Games', 'Movies', 'Music', 'Sports']
+
+CAT_TO_SUBCAT['Other'] = [
+    'General', 'Clothing', 'Life - Other', 'Gifts', 'Education', 'Insurance']
 
 
 DATA_PATH_PATTERN = os.path.normpath('./data/*_export.csv')
--- splitwise_bokeh.py
+++ splitwise_bokeh.py
@@ -10,99 +10,59 @@ from bokeh.core.properties import value
 from bokeh.models import Legend
 from bokeh.layouts import gridplot, row
 from bokeh.models.formatters import DatetimeTickFormatter, NumeralTickFormatter
+from bokeh.models.widgets import Panel, Tabs
 
 if __name__ == '__main__':
     expenses = data_preparation.get_expenses_df()
 
     # monthly by category data
-    monthlyByCategory = expenses.groupby('Category').resample('BMS').sum()
-    monthlyByCategoryUnstacked = monthlyByCategory.Cost.unstack(level=0)
-    monthlyByCategoryUnstacked = monthlyByCategoryUnstacked.fillna(0)
+    monthly_data = expenses.groupby('Category').resample('BMS').sum()
+    monthly_data = monthly_data.Cost.unstack(level=0)
+    monthly_data = monthly_data.fillna(0)
 
-    source = ColumnDataSource(monthlyByCategoryUnstacked)
+    source = ColumnDataSource(monthly_data)
     col_names = CAT_TO_SUBCAT.keys()
     # TODO make color-selection more understandable
     colors = [palette[20][i] for i in [1, 5, 3, 7, 15]]
     print(colors)
 
-    monthlyBars = figure(x_axis_label='Month', y_axis_label='Expenses', x_axis_type='datetime',
-                         plot_height=500, plot_width=800,
-                         tools="hover", tooltips="$name: @$name")
+    monthly_overview = figure(x_axis_label='Month', y_axis_label='Expenses', x_axis_type='datetime',
+                              plot_height=500, plot_width=800,
+                              tools="hover", tooltips="$name: @$name")
 
-    monthlyBars.vbar_stack(stackers=col_names, x='Date',
-                           width=2e9, color=colors,
-                           source=source, legend=[value(x) for x in col_names])
+    monthly_overview.vbar_stack(stackers=col_names, x='Date',
+                                width=2e9, color=colors,
+                                source=source, legend=[value(x) for x in col_names])
+    monthly_overview.y_range.start = 0
+    legend = monthly_overview.legend[0]
+    monthly_overview.add_layout(legend, 'right')
 
-    monthlyBars.y_range.start = 0
-
-    legend = monthlyBars.legend[0]
-    # monthlyBars.legend[0].plot = None
-    monthlyBars.add_layout(legend, 'right')
-
-
-    dateFormatter = DatetimeTickFormatter(months=["%m/%y"])
-    yFormatter = NumeralTickFormatter(format='0a')
-
-    # plots = []
-    # for i, col in enumerate(colNames):
-    #     p = figure(title=col, x_axis_label=None, y_axis_label=None, x_axis_type='datetime',
-    #                plot_height=200, plot_width=240,
-    #                    tools="hover, tap",tooltips='cost: @Clothing')# tooltips=f'cost: @{{{col}}}')
-    #     p.vbar(x='Date', top=col, width=2e9, color=colors[i], source=source)
-    #     p.title.text_font_size = '8pt'
-    #     p.xaxis.major_label_orientation = 0.8
-    #     plots.append(p)
-    #     p.xaxis.formatter = dateFormatter
-    #     p.yaxis.formatter = yFormatter
-
-    # grid1 = gridplot(plots, ncols=3)
-    # show(row(monthlyBars, grid1))
-
-    ################################################################################
-
-    # make hover tool data by iterating over expenses df and adding to source data
-    months_n = len(source.data['Date'])
-
-    for category in expenses['Category'].unique():
-        mask = expenses['Category'] == category
-        categoryData = expenses[mask]
-
-        monthlyEntries = pd.Series(index=source.data['Date'], data='')
-
-        for i in range(len(categoryData)):
-            description = categoryData.iloc[i]['Description']
-            cost = categoryData.iloc[i]['Cost']
-            entry = f'{description}: {cost}\n'
-
-            year = categoryData.index[i].year
-            month = categoryData.index[i].month
-            date = f'{year}-{month}'
-
-            monthlyEntries[date] += entry
-
-        source.add(monthlyEntries.to_list(), f'{category}_entries')
-        # print(source.data)
+    date_formatter = DatetimeTickFormatter(months=["%m/%y"])
+    y_formatter = NumeralTickFormatter(format='0a')
 
     ###################################################################################
 
-    plots = []
+    category_plots = []
+    panels = []
     for i, col in enumerate(col_names):
         entries_field = f'{col}_entries'
         p = figure(title=col, x_axis_label=None, y_axis_label=None, x_axis_type='datetime',
-                   plot_height=200, plot_width=240,
                    tools="hover, tap")
         p.vbar(x='Date', top=col, width=2e9, color=colors[i], source=source)
         p.title.text_font_size = '8pt'
         p.xaxis.major_label_orientation = 0.8
-        plots.append(p)
-        p.xaxis.formatter = dateFormatter
-        p.yaxis.formatter = yFormatter
-
+        p.xaxis.formatter = date_formatter
+        p.yaxis.formatter = y_formatter
         p.add_tools(HoverTool(tooltips=f'@{{{entries_field}}}'))
+
+        category_plots.append(p)
+        panels.append(Panel(child=p, title=col))
 
     # tooltips = f'@{{{entries_field}}}'
 
-    grid2 = gridplot(plots, ncols=3)
+    main_grid = gridplot(category_plots, ncols=3)
+    main_tab = Panel(child=monthly_overview, title='Overview')
+    panels.insert(0, main_tab)
 
-    # output
-    show(row(monthlyBars, grid2))
+    tabs = Tabs(tabs=panels)
+    show(tabs)
