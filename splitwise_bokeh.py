f4e93b80100826c7a54420f7ee1d89bc11e46f1e
tfausten <tobias.fausten@gmail.com>
2019-12-01 15:21:32 +0900

    cleanup and new tab-based layout

--- splitwise_bokeh.py
+++ splitwise_bokeh.py
@@ -7,7 +7,6 @@ from bokeh.plotting import figure
 from bokeh.io import show
 from bokeh.palettes import Category20 as palette
 from bokeh.core.properties import value
-from bokeh.models import Legend
 from bokeh.layouts import gridplot, row
 from bokeh.models.formatters import DatetimeTickFormatter, NumeralTickFormatter
 from bokeh.models.widgets import Panel, Tabs
@@ -22,45 +21,41 @@ if __name__ == '__main__':
 
     source = ColumnDataSource(monthly_data)
     col_names = CAT_TO_SUBCAT.keys()
+    date_formatter = DatetimeTickFormatter(months=["%m/%y"])
+    y_formatter = NumeralTickFormatter(format='0a')
+
     # TODO make color-selection more understandable
     colors = [palette[20][i] for i in [1, 5, 3, 7, 15]]
     print(colors)
 
-    monthly_overview = figure(x_axis_label='Month', y_axis_label='Expenses', x_axis_type='datetime',
-                              plot_height=500, plot_width=800,
-                              tools="hover", tooltips="$name: @$name")
+    monthly_overview = figure(x_axis_label='Month', y_axis_label='Expenses',
+                              x_axis_type='datetime', plot_height=500, plot_width=800,
+                              tools="hover, tap", tooltips="$name: @$name")
 
     monthly_overview.vbar_stack(stackers=col_names, x='Date',
                                 width=2e9, color=colors,
                                 source=source, legend=[value(x) for x in col_names])
     monthly_overview.y_range.start = 0
+    monthly_overview.yaxis.formatter = y_formatter
     legend = monthly_overview.legend[0]
     monthly_overview.add_layout(legend, 'right')
 
-    date_formatter = DatetimeTickFormatter(months=["%m/%y"])
-    y_formatter = NumeralTickFormatter(format='0a')
-
     ###################################################################################
 
-    category_plots = []
     panels = []
-    for i, col in enumerate(col_names):
-        entries_field = f'{col}_entries'
-        p = figure(title=col, x_axis_label=None, y_axis_label=None, x_axis_type='datetime',
-                   tools="hover, tap")
-        p.vbar(x='Date', top=col, width=2e9, color=colors[i], source=source)
-        p.title.text_font_size = '8pt'
+    for i, col_name in enumerate(col_names):
+        p = figure(x_axis_label='Month', y_axis_label='Expenses',
+                   x_axis_type='datetime', tools="hover, tap",
+                   tooltips=f"@Date{{%F}}: @{{{col_name}}}")
+        p.vbar(x='Date', top=col_name, width=2e9,
+               color=colors[i], source=source)
         p.xaxis.major_label_orientation = 0.8
         p.xaxis.formatter = date_formatter
         p.yaxis.formatter = y_formatter
-        p.add_tools(HoverTool(tooltips=f'@{{{entries_field}}}'))
 
-        category_plots.append(p)
-        panels.append(Panel(child=p, title=col))
+        panels.append(Panel(child=p, title=col_name))
 
-    # tooltips = f'@{{{entries_field}}}'
-
-    main_grid = gridplot(category_plots, ncols=3)
+    # main_grid = gridplot(category_plots, ncols=3)
     main_tab = Panel(child=monthly_overview, title='Overview')
     panels.insert(0, main_tab)
 
