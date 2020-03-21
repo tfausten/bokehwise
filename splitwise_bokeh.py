924f45019d1c6a919d14f083db10987497a59127
tfausten <tobias.fausten@gmail.com>
2020-03-21 18:21:31 +0900

    change to bokeh 2.0, stretch plots automatically, fix hovering in category plots, stack subcats for category plots

--- splitwise_bokeh.py
+++ splitwise_bokeh.py
@@ -1,62 +1,66 @@
 import data_preparation
 from constants import CAT_TO_SUBCAT
-from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
-from bokeh.io import show, curdoc
+from bokeh.io import curdoc
 from bokeh.palettes import Category20 as palette
-from bokeh.core.properties import value
-from bokeh.layouts import gridplot, row
 from bokeh.models.formatters import DatetimeTickFormatter, NumeralTickFormatter
 from bokeh.models.widgets import Panel, Tabs
 
 expenses = data_preparation.get_expenses_df()
 
 # monthly by category data
-monthly_data = expenses.groupby('Category').resample('BMS').sum()
-monthly_data = monthly_data.Cost.unstack(level=0)
-monthly_data = monthly_data.fillna(0)
-print('\nmonthly_data\n', monthly_data.head())
+category_data = expenses.groupby('Category').resample('BMS').sum()
+category_data = category_data.Cost.unstack(level=0)
+category_data = category_data.fillna(0)
+print('\nmonthly_data\n', category_data.head())
 
-source = ColumnDataSource(monthly_data)
-col_names = CAT_TO_SUBCAT.keys()
+category_source = ColumnDataSource(category_data)
+col_names = [str(n) for n in CAT_TO_SUBCAT.keys()]
 date_formatter = DatetimeTickFormatter(months=["%m/%y"])
 y_formatter = NumeralTickFormatter(format='0a')
 
 # TODO make color-selection more understandable
+# use cycle for the color palette
 colors = [palette[20][i] for i in [1, 5, 3, 7, 15]]
 print(colors)
 
-monthly_overview = figure(x_axis_label='Month', y_axis_label='Expenses',
-                          x_axis_type='datetime', plot_height=500, plot_width=800,
-                          tools="hover, tap", tooltips="$name: @$name")
 
+def create_default_fig():
+    fig = figure(x_axis_label='Month', y_axis_label='Expenses',
+                 x_axis_type='datetime', sizing_mode='stretch_both',
+                 tools="hover, tap", tooltips="$name: @$name")
+    fig.y_range.start = 0
+    fig.yaxis.formatter = y_formatter
+    return fig
+
+
+monthly_overview = create_default_fig()
 monthly_overview.vbar_stack(stackers=col_names, x='Date',
                             width=2e9, color=colors,
-                            source=source, legend=[value(x) for x in col_names])
-monthly_overview.y_range.start = 0
-monthly_overview.yaxis.formatter = y_formatter
-legend = monthly_overview.legend[0]
-monthly_overview.add_layout(legend, 'right')
+                            source=category_source, legend_label=col_names)
 
 panels = []
 panels.append(Panel(child=monthly_overview, title='Overview'))
 
+
+# make single-category plots, and group the data by subcategories
 for i, col_name in enumerate(col_names):
-    p = figure(x_axis_label='Month', y_axis_label='Expenses',
-               x_axis_type='datetime', tools="hover, tap",
-               tooltips=f"@Date{{%F}}: @{{{col_name}}}")
-    p.vbar(x='Date', top=col_name, width=2e9,
-           color=colors[i], source=source)
-    p.xaxis.major_label_orientation = 0.8
-    p.xaxis.formatter = date_formatter
-    p.yaxis.formatter = y_formatter
+    subcat_data = expenses[expenses['Category'] == col_name]
+    subcat_data = subcat_data.groupby('Subcategory').resample('BMS').sum()
+    subcat_data = subcat_data.Cost.unstack(level=0)
+    subcat_data = subcat_data.fillna(0)
+    # print('\nmonthly_data\n', subcat_data.head())
+    subcat_source = ColumnDataSource(subcat_data)
+    subcats = subcat_data.columns.tolist()
+
+    p = create_default_fig()
+    p.vbar_stack(stackers=subcats, x='Date', width=2e9,
+                 color=colors[i], line_color='black', source=subcat_source,
+                 legend_label=subcats)
 
     panels.append(Panel(child=p, title=col_name))
-
-# main_grid = gridplot(category_plots, ncols=3)
 
 tabs = Tabs(tabs=panels)
 curdoc().title = 'Bokehwise'
 curdoc().add_root(tabs)
-
-# show(tabs)
